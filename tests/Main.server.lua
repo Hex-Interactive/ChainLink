6ac376835136b11345a3022a9644706e06b0d624
Paul <43885283+Crystalflxme@users.noreply.github.com>
2023-11-30 01:54:04 -0500

    Add lexer decimal support

--- tests/Main.server.lua
+++ tests/Main.server.lua
@@ -2,3 +2,4 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Chain = require(ReplicatedStorage.Packages.ChainLink.Chain)
 Chain.Execute(`Name(true,"hello \\" there",1+2,-33);TestHere()`)
+Chain.Execute(`NumberTesting(23.64,-0.4,76)`)
