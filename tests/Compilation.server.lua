05373b103b3532dbff5f41b13b632645a651174e
Paul <43885283+Crystalflxme@users.noreply.github.com>
2023-12-03 18:02:55 -0500

    Revise testing function

--- tests/Compilation.server.lua
+++ tests/Compilation.server.lua
@@ -1,12 +1,14 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Chain = require(ReplicatedStorage.Packages.ChainLink.Chain)
 
-local function test(source: string)
-	local ast, lexemes = Chain.Compile(source)
-	local success = ast ~= nil and lexemes ~= nil
-	local sep = string.rep(".", 25)
+local SEP = string.rep("-", 25)
 
-	print(`{sep}\nTest success: {string.upper(tostring(success))}\nLexemes: {lexemes}\nAST: {ast}\n{sep}\n`)
+local function test(source: string)
+	local errSuccess, astOrErr, lexemes = pcall(Chain.Compile, source)
+	local testSuccess = astOrErr ~= nil and lexemes ~= nil
+	local success = errSuccess and testSuccess
+	local results = if success then `Lexemes: {lexemes}\nAST: {astOrErr}` else `Error message: "{astOrErr}"`
+	print(`{SEP}\nTest successful: {string.upper(tostring(success))}\n{results}\n{SEP}\n`)
 end
 
 test(`Name(true,"hello \\" there",1+2,-33);TestHere()`)
