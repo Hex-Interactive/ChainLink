f80dbc77328676913dd2e42cc05012909ed29023
Paul <43885283+Crystalflxme@users.noreply.github.com>
2023-12-03 18:57:26 -0500

    Revise test output

--- tests/Compilation.server.lua
+++ tests/Compilation.server.lua
@@ -1,4 +1,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local HttpService = game:GetService("HttpService")
+
 local Chain = require(ReplicatedStorage.Packages.ChainLink.Chain)
 
 local SEP = string.rep("-", 25)
@@ -7,7 +9,10 @@ local function test(source: string)
 	local errSuccess, astOrErr, lexemes = pcall(Chain.Compile, source)
 	local testSuccess = astOrErr ~= nil and lexemes ~= nil
 	local success = errSuccess and testSuccess
-	local results = if success then `Lexemes: {lexemes}\nAST: {astOrErr}` else `Error message: "{astOrErr}"`
+	local results = if success
+		then `Lexemes: {HttpService:JSONEncode(lexemes)}\nAST: {HttpService:JSONEncode(astOrErr)}`
+		else `Error message: "{astOrErr}"`
+
 	print(`{SEP}\nTest successful: {string.upper(tostring(success))}\n{results}\n{SEP}\n`)
 end
 
